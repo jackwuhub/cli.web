3f6383120288f26f02060b51d6d7f112555c0727
吳廣帥 <15809812381@163.com>
2023-08-28 23:47:06 +0800

    chore: 构建map

--- bin/index.js
+++ bin/index.js
@@ -4,13 +4,13 @@ const { showMain } = require('../lib/message/index')
 const programRegister = require('../lib/program/index')
 const prompts = require('../lib/prompts/index')
 const repo = require('../lib/repo/index')
+const config = require('../lib/repo/config')
 showMain() // 显示文字
 // 注册指令
 programRegister(program,{
     create: async (projectName) => { // 创建command
-        const promptsForm =  await prompts(projectName)
+        const promptsForm =  await prompts(config)
         repo({projectName,...promptsForm}) // 校验参数, 准备下载
     }
 })
-
 
