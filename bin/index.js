059099bcb905f9e50cf58aad42739133722c8ade
吳廣帥 <15809812381@163.com>
2023-08-29 14:37:15 +0800

    dev: 构建更新

--- bin/index.js
+++ bin/index.js
@@ -1,16 +1,37 @@
 #! /usr/bin/env node
 const {program} = require("commander")
-const { showMain } = require('../lib/message/index')
-const programRegister = require('../lib/program/index')
-const prompts = require('../lib/prompts/index')
-const repo = require('../lib/repo/index')
-const config = require('../lib/repo/config')
-showMain() // 显示文字
+const { showMain, showDirTree} = require('../lib/message')
+const programRegister = require('../lib/program')
+const {create,confirmCreate,confirmRemake} = require('../lib/prompts')
+const download = require('../lib/repo')
+const config = require('../lib/config')
+const {directoryExists, deleteFile} = require("../lib/file");
+const updateChk = require("../lib/upgrade");
 // 注册指令
 programRegister(program,{
-    create: async (projectName) => { // 创建command
-        const promptsForm =  await prompts(config)
-        repo({projectName,...promptsForm}) // 校验参数, 准备下载
+    // 创建项目
+    create: async (projectName) => {
+        showMain() // 显示文字
+        const { repo } =  await create(config) // form信息
+        const path = `./${projectName}`
+        if(directoryExists(path)){ // 同名目录
+            const res = await confirmRemake() // 是否删除
+            if(res) await deleteFile(path)
+        }
+        const res = await confirmCreate()
+        if(res) { // 校验参数, 准备下载
+            const res = await download({projectName,repo})
+            showDirTree(res)
+        }
+    },
+    // 查看目录结构
+    dir: async (dir) => {
+        if(!dir) dir = './'
+        showDirTree(dir)
+    },
+    // 手动更新项目
+    upgrade: async () => {
+        updateChk()
     }
 })
 
