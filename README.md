4dd3a4d9bb197e75e8790d0ce491fbae80f8f3d6
吳廣帥 <15809812381@163.com>
2023-08-29 16:15:32 +0800

    dev: 更新在线config配置

--- README.md
+++ README.md
@@ -4,19 +4,14 @@
 前端项目脚手架
 
 #### 软件架构
-软件架构说明
+安装后命令 ldgy-cli
 
+#### 包含command
+> 1. ldgy-cli create projectName 创建项目
+> 2. ldgy-cli upgrade 更新包版本
+> 3. ldgy-cli showdir path 查看项目结构
 
-#### 安装教程
-
-1.  xxxx
-2.  xxxx
-3.  xxxx
-
-#### 使用说明
-
-1.  xxxx
-2.  xxxx
-3.  xxxx
-
-
+#### repo-config 
+> 1. 只有最终选项填写value, 内容为仓库url
+> 2. 不填写value, 没有choices 则禁用该选项
+> 3. value格式为: branch::repoUrl eg: master::https://gitee.com/wuguangshuai/template.admin.vue3.tdsign.git
--- bin/index.js
+++ bin/index.js
@@ -3,32 +3,32 @@ const {program} = require("commander")
 const { showMain, showDirTree} = require('../lib/message')
 const programRegister = require('../lib/program')
 const {create,confirmCreate,confirmRemake} = require('../lib/prompts')
-const download = require('../lib/repo')
-const config = require('../lib/config')
+const {gitRepoFetch, getRepoConfig} = require('../lib/repo')
 const {directoryExists, deleteFile} = require("../lib/file");
 const updateChk = require("../lib/upgrade");
+const chalk = require("chalk");
+
+
 // 注册指令
 programRegister(program,{
     // 创建项目
     create: async (projectName) => {
         showMain() // 显示文字
-
         const hasUpgrade = await updateChk() // 检查更新
         if(hasUpgrade) return // 拦截
-
-
-        const { repo } =  await create(config) // form信息
+        // 加载
+        const { repo } =  await create(await getRepoConfig(true)) // form信息
         const path = `./${projectName}`
         if(directoryExists(path)){ // 同名目录
             const res = await confirmRemake() // 是否删除
             if(res) await deleteFile(path) // 删除目录
             else return
         }
-
+        // 确认
         const res = await confirmCreate() // 确认创建
         if(res) { // 校验参数, 准备下载
-            const filePath = await download({projectName,repo})
-            if(filePath) showDirTree(filePath)
+            const filePath = await gitRepoFetch({projectName,repo})
+            console.log(chalk.white('项目路径:') + chalk.green(filePath))
         }
 
     },
